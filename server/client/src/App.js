227f028da6f2f9c087a879866a57463e49160272
Dylan Morison <dylan@g.morison.org>
2021-01-11 22:26:59 -0800

    added react, concurrently, and client modules.

---
+++ server/client/src/App.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react";
+import { Navbar } from "./components/layout/Navbar";
+import { Landing } from "./components/layout/Landing";
+
+const App = () => {
+	return (
+		<Fragment>
+			<Navbar />
+			<Landing />
+		</Fragment>
+	);
+};
+
+export default App;

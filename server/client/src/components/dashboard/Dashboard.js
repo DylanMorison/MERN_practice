9832374b7c46019ca3da41d662c2a72f9b6004a4
Dylan Morison <dylan@g.morison.org>
2021-01-13 17:05:27 -0800

    begining profile redux construction

---
+++ server/client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Dashboard() {
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+Dashboard.prototypes = {
+
+}
+
+export default Dashboard

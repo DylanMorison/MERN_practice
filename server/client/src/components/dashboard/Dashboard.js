de23664ea41affc2ba22882a141b90cdb9f7ff84
Dylan Morison <dylan@g.morison.org>
2021-01-13 18:38:24 -0800

    added clear profile on logout to redux

--- server/client/src/components/dashboard/Dashboard.js
+++ server/client/src/components/dashboard/Dashboard.js
@@ -4,12 +4,26 @@ import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
 
-const Dashboard = ({ getCurrentProfile, auth, profile: { profile, loading } }) => {
+const Dashboard = ({
+	getCurrentProfile,
+	auth: { user },
+	profile: { profile, loading }
+}) => {
 	useEffect(() => {
 		getCurrentProfile();
 	}, []);
 
-	return loading && profile === null ? <Spinner /> : <Fragment>test</Fragment>;
+	return loading && profile === null ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<h1 className="large text-primary">Dashboard</h1>
+			<p className="lead">
+				<i className="fas fa-user"></i> Welcome {user && user.name}
+			</p>
+			{profile !== null ? <Fragment>has</Fragment> : <Fragment>has not</Fragment>}
+		</Fragment>
+	);
 };
 
 Dashboard.prototypes = {
